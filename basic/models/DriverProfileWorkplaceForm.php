3c928a9ea302f2c8c78ab57c2e777cd840958c1c
Egor Nagornov <nagornov_e@bk.ru>
2020-10-25 13:41:18 +0000

    Отображение списков, сохранение и обоновление предыдущего места работы

--- basic/models/DriverProfileWorkplaceForm.php
+++ basic/models/DriverProfileWorkplaceForm.php
@@ -15,7 +15,8 @@ class DriverProfileWorkplaceForm extends Model
 	public $post;
 	public $action;
 	public $dismissal;
-	public $guarantor;	
+	public $guarantor;
+	public $selectedWorkId;
 
 	public function rules() 
 	{
@@ -26,6 +27,7 @@ class DriverProfileWorkplaceForm extends Model
 			['post', 'required', 'message' => 'Заполните поле "Должность*:"' ],
 			['action', 'required', 'message' => 'Заполните поле "Содержание деятельности*:"' ],
 			['dismissal', 'required', 'message' => 'Заполните поле "Причина увольнения*:"' ],
+			['selectedWorkId', 'required', 'message' => 'Заполните поле "Id"' ],
 			['guarantor', 'required', 'message' => 'Заполните поле "Кто может дать рекомендации с даннного места
 			работы (ФИО, контакт для связи)*:"' ],			
 		];
@@ -44,39 +46,83 @@ class DriverProfileWorkplaceForm extends Model
 			->bindValue(':id', Yii::$app->user->identity->id)
 			->queryAll();
 
+		if(sizeof($list)) {					
+			$list[0]['sdate'] = date('d.m.Y', strtotime($list[0]['sdate']));
+			$list[0]['edate'] = date('d.m.Y', strtotime($list[0]['edate']));
+			return $list[0];
+        } else {	
+			return sizeof($list) ? $list[0]:null;			
+		}		
+	}
+
+	public function getDriverProfileWorkplaceList () 
+	{
+		$list = ($this->db_conn->createCommand("select * from workplace where did=:id"))
+			->bindValue(':id', Yii::$app->user->identity->id)
+			->queryAll();
+
 		if(sizeof($list)) {
 			foreach ($list as &$value) {			
 				$value['sdate'] = date('d.m.Y', strtotime($value['sdate']));
 				$value['edate'] = date('d.m.Y', strtotime($value['edate']));		
 			}
-        }		
 
-		return sizeof($list) ? $list[0]:null;
+			return $list;
+        } else {			
+			return sizeof($list) ? $list[0]:null;			
+		}		
 	}
 
 	public function saveDriverProfileWorkplace() 
 	{
-		$this->db_conn->createCommand("update workplace set sdate=:workStartDate, edate=:workEndDate, company=:company, post=:post,   
-		action=:action, dismissal=:dismissal, guarantor=:guarantor where did=:id",
-			[
-				':workStartDate'   => null,
-				':workEndDate'     => null,
-				':company'   	   => null,
-				':post'   		   => null,
-				':action'          => null,
-				':dismissal'       => null,					
-				':guarantor'       => null,
-				':id'              => null,
-			])
-		->bindValue(':workStartDate', date_format(date_create_from_format('d.m.Y', $this->workStartDate), 'Y-m-d'))		
-		->bindValue(':workEndDate',   date_format(date_create_from_format('d.m.Y', $this->workEndDate), 'Y-m-d') )
-		->bindValue(':company',  $this->company)
-		->bindValue(':post',   $this->post)
-		->bindValue(':action',   $this->action)
-		->bindValue(':dismissal',   $this->dismissal)
-		->bindValue(':guarantor',   $this->guarantor)
-		->bindValue(':id',          Yii::$app->user->identity->id)
-		->execute();		
+		if($this->selectedWorkId != 0) {
+			$this->db_conn->createCommand("update workplace set sdate=:workStartDate, edate=:workEndDate, company=:company, post=:post,   
+			action=:action, dismissal=:dismissal, guarantor=:guarantor where did=:id and id=:selectedWorkId",
+				[
+					':workStartDate'   => null,
+					':workEndDate'     => null,
+					':company'   	   => null,
+					':post'   		   => null,
+					':action'          => null,
+					':dismissal'       => null,					
+					':guarantor'       => null,
+					':selectedWorkId'  => null,
+					':id'              => null,
+				])
+			->bindValue(':workStartDate', date_format(date_create_from_format('d.m.Y', $this->workStartDate), 'Y-m-d'))		
+			->bindValue(':workEndDate',   date_format(date_create_from_format('d.m.Y', $this->workEndDate), 'Y-m-d') )
+			->bindValue(':company',        $this->company)
+			->bindValue(':post',           $this->post)
+			->bindValue(':action',         $this->action)
+			->bindValue(':dismissal',      $this->dismissal)
+			->bindValue(':guarantor',      $this->guarantor)
+			->bindValue(':selectedWorkId', $this->selectedWorkId)
+			->bindValue(':id',             Yii::$app->user->identity->id)
+			->execute();
+		} else {
+			$this->db_conn->createCommand("insert into workplace (sdate, edate, company, post, action, dismissal, guarantor, did)
+			 values (:workStartDate, :workEndDate, :company, :post, :action, :dismissal, :guarantor, :id)",
+				[
+					':workStartDate'   => null,
+					':workEndDate'     => null,
+					':company'   	   => null,
+					':post'   		   => null,
+					':action'          => null,
+					':dismissal'       => null,					
+					':guarantor'       => null,			
+					':id'              => null,
+				])
+			->bindValue(':workStartDate', date_format(date_create_from_format('d.m.Y', $this->workStartDate), 'Y-m-d'))		
+			->bindValue(':workEndDate',   date_format(date_create_from_format('d.m.Y', $this->workEndDate), 'Y-m-d') )
+			->bindValue(':company',        $this->company)
+			->bindValue(':post',           $this->post)
+			->bindValue(':action',         $this->action)
+			->bindValue(':dismissal',      $this->dismissal)
+			->bindValue(':guarantor',      $this->guarantor)			
+			->bindValue(':id',             Yii::$app->user->identity->id)
+			->execute();
+		}		
+		
 	}
 
 }
